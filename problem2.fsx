055f0c15cf849c69b8f82b94d7a7efde927eed1a
Erlend Røsjø <erlend@erlend.net>
2015-07-03 12:47:12 +0200

    Removed List.ofSeq, replaced List.fold with Seq.fold

--- problem2.fsx
+++ problem2.fsx
@@ -1,5 +1,5 @@
 let fibs = (1I, 2I) |> Seq.unfold (fun (a, b) -> Some(a, (b, a + b) ) );;
-let sum = Seq.takeWhile (fun x -> x < 4000000I) fibs |> Seq.filter(fun x -> x.IsEven) |> List.ofSeq |> List.fold (+) 0I
+let sum = Seq.takeWhile (fun x -> x < 4000000I) fibs |> Seq.filter(fun x -> x.IsEven) |> Seq.fold (+) 0I
 
 [<EntryPoint>]
 let main argv = 
